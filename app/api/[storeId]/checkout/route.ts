b068afbb33eb38cb810070118271ffa8b141f045
Travier.Thomas <travier.thomas@cci.com>
2024-01-29 22:01:44 +0100

    tentative correction route lors du cancel du checkout qui redirige pas vers le store

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -7,76 +7,74 @@ import prismadb from "@/lib/prismadb"
 const corsHeaders = {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-    "ACCESS-Control-Allow-Headers": "Content-Type, Authorization"
-}
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
+  };
+  
+  export async function OPTIONS() {
+    return NextResponse.json({}, { headers: corsHeaders });
+  }
 
-export async function OPTIONS(){
-    return NextResponse.json({}, { headers: corsHeaders })
-}
-
-export async function POST(
+  export async function POST(
     req: Request,
-    { params }:{ params: { storeId : string } }
-){
-    const { productIds } = await req.json()
+    { params }: { params: { storeId: string } }
+  ) {
+    const { productIds } = await req.json();
 
     if(!productIds || productIds.length === 0){
         return new NextResponse("Des id de produits sont requis", {status : 400})
     }
 
     const products = await prismadb.product.findMany({
-        where:{
-            id:{
-                in: productIds
-            }
+        where: {
+          id: {
+            in: productIds
+          }
         }
-    })
+      });
 
-    const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = []
+      const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [];
 
     products.forEach((product) => {
         line_items.push({
             quantity:1,
             price_data:{
                 currency:'EUR',
-                product_data:{
-                    name: product.name
-                },
-                unit_amount: product.price.toNumber()*100
-            }
-            
-        })
-    })
+                product_data: {
+                    name: product.name,
+                  },
+                  unit_amount: product.price.toNumber() * 100
+                }
+              });
+            });
     const order = await prismadb.order.create({
-        data:{
-            storeId: params.storeId,
-            isPaid: false,
-            orderItems:{
-                create: productIds.map((productId: string) => {
-                    product:{
-                        connect:{
-                            id: productId
-                        }
-                    }
-                })
-            }
+        data: {
+          storeId: params.storeId,
+          isPaid: false,
+          orderItems: {
+            create: productIds.map((productId: string) => ({
+              product: {
+                connect: {
+                  id: productId
+                }
+              }
+            }))
+          }
         }
-    })
+      });
 
-    const session = await stripe.checkout.sessions.create({
+      const session = await stripe.checkout.sessions.create({
         line_items,
-        mode: "payment",
-        billing_address_collection:"required",
-        phone_number_collection:{
-            enabled: true,
-
+        mode: 'payment',
+        billing_address_collection: 'required',
+        phone_number_collection: {
+          enabled: true,
         },
         success_url: `${process.env.FRONTEND_STORE_URL}/cart?success=1`,
         cancel_url: `${process.env.FRONTEND_STORE_URL}/cart?canceled=1`,
-        metadata:{
-            orderId: order.id
-        }
-    })
+        metadata: {
+          orderId: order.id
+        },
+      });
 
     return NextResponse.json({url: session.url},{
         headers: corsHeaders
