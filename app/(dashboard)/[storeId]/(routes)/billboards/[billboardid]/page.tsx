d567d5be9c10d7d554d3456b2b50893bc4968c17
Travier.Thomas <travier.thomas@cci.com>
2024-01-31 16:09:58 +0100

    mise en com de la tentative de correction

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardid]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardid]/page.tsx
@@ -21,6 +21,32 @@ const BillboardPage = async ({
       </div>
     );
   }
+
+  
+// const BillboardPage = async ({
+//   params
+// }: {
+//   params: { billboardId: string }
+// }) => {
+//   if (!params.billboardId) {
+//     // Gérer le cas où l'ID n'est pas défini, par exemple, rediriger vers une page d'erreur.
+//     return <div>Erreur : ID de billboard non défini</div>;
+//   }
+
+//   const billboard = await prismadb.billboard.findUnique({
+//     where: {
+//       id: params.billboardId
+//     }
+//   });
+
+//   return (
+//     <div className="flex-col">
+//       <div className="flex-1 space-y-4 p-8 pt-6">
+//         <BillboardForm initialData={billboard} />
+//       </div>
+//     </div>
+//   );
+// };
   
 
 export default BillboardPage;
